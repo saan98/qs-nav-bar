d54069600582a27b38fc04cf19150445467ced13
Ishan Shrestha <ishanstha1998@gmail.com>
2023-09-22 18:56:25 +0545

    nav-skeleton

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ document.addEventListener("DOMContentLoaded", function () {
     const toggleButton = document.getElementById("toggle-navbar");
     const navbar = document.getElementById("navbar");
     const whiteBackground = document.querySelector(".white-background");
-   const rightBackground = document.querySelector(".right-background");
+    const blackBackground = document.querySelector(".black-background");
 
     toggleButton.addEventListener("click", function () {
         if (navbar.style.top === "0px") {
             // Close the navbar
             navbar.style.top = "-100%";
-            whiteBackground.style.left = "-50%"; 
-            rightBackground.style.right = "-50%"; 
+            whiteBackground.style.left = "-100%"; 
+            blackBackground.style.right = "-100%"; 
         } else {
             // Open the navbar
             navbar.style.top = "0px";
             whiteBackground.style.left = "0"; 
-            rightBackground.style.right = "0"; 
+            blackBackground.style.right = "0"; 
         }
     });
 });
